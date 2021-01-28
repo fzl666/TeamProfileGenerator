706d82fc6e2ba75dcc2c7becc8f42dc3a3aed26e
Luwenxi Song <songl652@newschool.edu>
2021-01-28 14:30:33 -0500

    command qs

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
 const fs = require('fs')
-const inquirer = require('inquirer')
 const jest = require('jest')
+const inquirer = require('inquirer')
+
+
+    inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "string",
+        message: "Employee's name",
+      },
+      {
+        name: "id",
+        type: "string",
+        message: "Employee's id",
+      },
+      {
+        name: "email",
+        type: "string",
+        message: "Employee's email",
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "Employee's role",
+        choices: ["Engineer", "Intern", "Manager"],
+      },
+     ]).then((answer) => {
+        switch (answer.role){
+            case 'Engineer':
+                inquirer
+                .prompt([
+                  {
+                    name: "github",
+                    type: "string",
+                    message: "Github username",
+                  },  
+                ])
+                break;
+            case 'Intern':
+                inquirer
+                .prompt([
+                  {
+                    name: "school",
+                    type: "string",
+                    message: "Schools's name",
+                  }
+                ])
+                break;
+            case 'Manager':
+                inquirer
+               .prompt([
+               {
+               name: "office",
+               type: "string",
+               message: "office number",
+               }
+            ])
+            break;
+        }
+     })
---
+++ tests/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require("../lib/Employee")
+
+discribe('Employee'), () => {
+    describe('getName', () => {
+        it("Should prompt user to type a string"), ()=>{
+            
+        }
+    })
+}
